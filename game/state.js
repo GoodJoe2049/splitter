c5b276da3e48efac7f405e2442d98f2d06055ff6
Marcos Rodriguez <marcosrod171@gmail.com>
2024-04-25 23:12:54 -0500

    more setup, broken bar

---
+++ game/state.js
@@ -0,0 +1,6 @@
+
+export const state = {
+    heatLevel: 0,   //the difficulty level
+    totalEnergy: 0, //energy generated through the entire run
+    lifeBar: 10000, //current energy to stay alive
+};
--- main.js
+++ main.js
@@ -15,5 +15,15 @@ const drawGameWindow = () => {
     document.getElementById("game-window-container").style.height = windowHeight + "px";
 };
 
+const startGame = () => {
+    setInterval(function(){
+        let tempwidth = document.getElementById("life-bar").clientWidth; //redo this and put stuff in classes
+        tempwidth = tempwidth - Math.round(0.01 * tempwidth);
+        console.log(tempwidth);
+        document.getElementById("life-bar").style.width = tempwidth + "px";
+    }, 100);
+};
+
 onresize();
 drawGameWindow();
+startGame();
